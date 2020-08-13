64011caa7c735e75b0d7db8735508ca886fa0083
Bartosz <bartosz.zelek@protonmail.com>
2020-08-13 23:19:42 +0200

    Second header

--- ckziusroda/frontend/src/components/layout/Header.js
+++ ckziusroda/frontend/src/components/layout/Header.js
@@ -7,8 +7,9 @@ export class Header extends Component {
       <div className="d-none d-sm-block">
         <nav className="navbar navbar-expand-lg bg-primary d-flex justify-content-around">
           <ul className="navbar-nav">
-            <li className="">
-              {mailIcon}
+            <li className="nav-item">
+              {/* {mailIcon} */}
+              📧
               <a
                 href="mailto:"
                 style={{ textDecoration: "none" }}
@@ -17,7 +18,8 @@ export class Header extends Component {
                 sekretariat@ckziusroda.edu.pl
               </a>
               <span className="border-left ml-3 mr-3"></span>
-              {telephoneIcon}
+              {/* {telephoneIcon} */}
+              📞
               <a
                 href="tel:+"
                 style={{ textDecoration: "none" }}
@@ -31,7 +33,9 @@ export class Header extends Component {
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle text-light border border-light p-2"
-                style={{ textDecoration: "none" }}
+                style={{
+                  textDecoration: "none",
+                }}
                 href="#"
                 id="navbarDropdownMenuLink"
                 role="button"
@@ -46,15 +50,57 @@ export class Header extends Component {
                 aria-labelledby="navbarDropdownMenuLink"
               >
                 <a className="dropdown-item" href="#">
-                  Action
+                  Facebook
                 </a>
+                <div class="dropdown-divider"></div>
                 <a className="dropdown-item" href="#">
-                  Another action
+                  Instagram
                 </a>
+                <div class="dropdown-divider"></div>
                 <a className="dropdown-item" href="#">
-                  Something else here
+                  Dziennik elektroniczny
+                </a>
+                <div class="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Powiat Średzki
+                </a>
+                <div class="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Biuletyn Informacji Publicznej
+                </a>
+                <div class="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Plan lekcji
+                </a>
+                <div class="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Zaloguj - administrator
                 </a>
               </div>
+            </li>
+          </ul>
+        </nav>
+
+        <nav className="navbar navbar-expand-lg justify-content-center">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <img
+                src="https://ckziusroda.edu.pl/wp-content/uploads/2019/10/logo16102019_v1.jpg"
+                alt="Logo CKZiU"
+                className="img-fluid"
+              />
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-5">
+            <li className="nav-item dropdown">
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2 border border-secondary rounded"
+                  type="search"
+                  placeholder="Szukaj 🔎"
+                  aria-label="Search"
+                ></input>
+              </form>
             </li>
           </ul>
         </nav>

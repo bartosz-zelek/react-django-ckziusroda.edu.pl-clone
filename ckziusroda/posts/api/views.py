4cbb99a90ba096b73d1f6428396ac584ce6ace3d
Bartosz <bartosz.zelek@protonmail.com>
2020-08-16 14:01:14 +0200

    Show just publicated news

--- ckziusroda/posts/api/views.py
+++ ckziusroda/posts/api/views.py
@@ -4,7 +4,6 @@ from .serializers import NewsSerializer
 
 
 class NewsListView(generics.ListAPIView):
-    # queryset = News.objects.all()[:6]
     def get_queryset(self):
         return News.objects.filter(public=True)[:6]
 
